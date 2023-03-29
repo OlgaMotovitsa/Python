d029873237d0c4e4c8a462af6a94203cd63372c9
DFIT Ольга <Olga.Motovitsa@gmail.com>
2023-03-30 00:46:48 +0300

    add Homework4

--- Seminar4/randomText.py
+++ Seminar4/randomText.py
@@ -1,4 +1,5 @@
 import random
+import string
 
 my_random_text = []
 
@@ -6,6 +7,7 @@ for i in range(20):
     word = ''
     for j in range(random.randint(4,8)):
         word += random.choice(string.ascii_lowercase)
-    my_random_text.append(word)
+    my_random_text.append(word + str(random.choice(['.', ',', '?'])
+    if random.choice([0,0,1]) else '')) # типо слитно с пред строчкой
 
 print(' '.join(my_random_text))
