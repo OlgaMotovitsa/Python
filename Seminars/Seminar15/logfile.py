db4b89a99d4c92635062368fd1a6663f15a750a1
DFIT Ольга <Olga.Motovitsa@gmail.com>
2023-11-04 15:54:42 +0300

    add Seminar15

---
+++ Seminars/Seminar15/logfile.py
@@ -0,0 +1,29 @@
+# Задание №1
+# Напишите программу, которая использует модуль logging для
+# вывода сообщения об ошибке в файл.
+# Например отлавливаем ошибку деления на ноль.
+
+
+import logging
+
+logging.basicConfig(filename='logs.log', filemode='w', encoding='utf-8', level=logging.ERROR)
+logger = logging.getLogger(__name__)
+
+
+def my_func(storage, key, value=None):
+    try:
+        return storage[key]
+    except KeyError:
+        logging.error(f'отсутствует ключ {key} в словаре {storage}')
+        return value
+
+
+f = {'f': '1', 'd': '3', 's': '5'}
+
+print(my_func(f,'a', 2))
+
+
+
+
+
+
