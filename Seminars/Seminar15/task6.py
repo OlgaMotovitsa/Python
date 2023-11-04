1182be67e72d165ac926d7aa9b81d3e59471a71f
DFIT Ольга <Olga.Motovitsa@gmail.com>
2023-11-04 16:06:14 +0300

    add Seminar15

---
+++ Seminars/Seminar15/task6.py
@@ -0,0 +1,26 @@
+# Задание №6
+# Напишите код, который запускается из командной строки и получает на вход
+# путь до директории на ПК.
+# Соберите информацию о содержимом в виде объектов namedtuple.
+# Каждый объект хранит:
+# ○ имя файла без расширения или название каталога,
+# ○ расширение, если это файл,
+# ○ флаг каталога,
+# ○ название родительского каталога.
+# В процессе сбора сохраните данные в текстовый файл используя
+# логирование
+
+import pathlib
+from collections import namedtuple
+
+DIRSUBJECT = namedtuple('DIRSUBJECT',['file_name', 'ext', 'flag', 'name_dir'])
+
+def dir_info(path):
+    path = pathlib.Path(path)
+    new_list = []
+    for file in path.iterdir():
+        new_list.append(DIRSUBJECT(file.name, file.suffix, file.is_dir(), file.parent))
+    return new_list
+
+print(dir_info('C:\Users\Оля\OneDrive\Рабочий стол\Учеба\Python'))
+
